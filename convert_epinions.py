3e2c1eb472cb2e4f5e5e7e3d9e0d230dc259ff08
Jim <jim32990@gmail.com>
2014-11-12 22:07:54 -0800

    Remove H

--- convert_epinions.py
+++ convert_epinions.py
@@ -4,7 +4,8 @@ H = snap.TIntStrH()
 
 G = snap.LoadEdgeList(snap.PNEANet, "data/epinions/soc-sign-epinions.txt", 0, 1)
 
-for i in range(G.GetNodes()):
-    H.AddDat(i, str(i))
+
+#for i in range(G.GetNodes()):
+#    H.AddDat(i, str(i))
 
 snap.SaveGViz(G, "epinions.txt", "Directed Epinions Graph", True, H)
